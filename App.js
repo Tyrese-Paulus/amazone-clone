ce47388e745deb4db0f8df3070fc072c76b87522
tyrese-paulus <tpwebdevelopment11@gmail.com>
2021-04-08 21:29:53 +0200

    initial commit, navbar done and react router setup

---
+++ App.js
@@ -0,0 +1,29 @@
+import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Header from './Header'
+
+function App() {
+  return (
+    <Router>
+      <div className='app'>
+        <Switch>
+          <Route path='/checkout'>
+            <h1>CHECKOUT</h1>
+          </Route>
+
+          <Route path='/login'>
+            <h1>LOGIN</h1>
+          </Route>
+
+          <Route path='/'>
+            <Header />
+            <h1>HOME</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
